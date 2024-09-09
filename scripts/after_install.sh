0b2b8b3221a3fe5b4ed2d134493b5066c8fb534b
progounick <79576643+progounick@users.noreply.github.com>
2024-09-09 23:46:55 +0300

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 # after_install.sh
 
+# Variables
+DEPLOY_DIR="/opt/algotech/platform"
+JAR_FILE="$DEPLOY_DIR/*.jar"
+
+# Change ownership of the deployment directory and files to the ubuntu user
+chown -R algouser:algouser $DEPLOY_DIR
+
+# Set executable permissions for the jar file (if needed)
+#chmod +x $JAR_FILE
+
+echo "Ownership and permissions updated."
+
 # Start the application if it's a service or script
 echo "Starting application..."
 sudo systemctl start algotech-platform.service  # Replace 'my-app' with your app's service if applicable
