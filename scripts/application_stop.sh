23c52989792a93daef704edea08a00e600bab159
progounick <79576643+progounick@users.noreply.github.com>
2024-09-09 23:35:57 +0300

    Update application_stop.sh

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -3,4 +3,4 @@
 
 # Stop any existing application process (adjust for your application)
 echo "Stopping existing application..."
-sudo systemctl stop algotech-platform.service || true  # Replace 'my-app' with your app's service if applicable
+sudo systemctl stop algotech-platform.service || true
